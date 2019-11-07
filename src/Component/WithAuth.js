82a85737c7049ce833a95a576ff884b887804279
rizkypjt <rizkyhidayatpanjaitan97@gmail.com>
2019-11-07 16:13:37 +0700

    adding component WithAuth

---
+++ src/Component/WithAuth.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import AuthService from './AuthService';
+
+function WithAuth(AuthComponent) {
+    const Auth = new AuthService();
+    return class AuthWrapped extends React.Component{
+        constructor(){
+            super();
+            this.state = {
+                user:null
+            }
+        }
+        componentWillMount() {
+            if(!Auth.loggedIn())
+                this.props.history.replace('/login')
+            else{
+                try{
+                    const profile = Auth.getProfile();
+                    this.setState({
+                        user:profile
+                    })
+                }
+                catch(err){
+                    Auth.logout();
+                    this.props.history.replace('/login')
+                }
+            }
+        }
+        render(){
+            if(this.state.user){
+                return(
+                    <AuthComponent 
+                        {...this.props} 
+                        {...this.state} 
+                    />
+                )
+            }else{
+                return null;
+            }
+        }
+    }
+}
+
+export default WithAuth;
